ac4f1a4497713c10f34cf84786519f94073d819e
heymoko <gozila__adm@naver.com>
2022-05-10 17:56:09 +0900

    fin

--- app/src/main/java/com/project/imagesearch/MainActivity.kt
+++ app/src/main/java/com/project/imagesearch/MainActivity.kt
@@ -1,15 +1,33 @@
 package com.project.imagesearch
 
+import android.Manifest
 import android.annotation.SuppressLint
+import android.app.WallpaperManager
+import android.content.ContentValues
 import android.content.Context
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.drawable.Drawable
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract
+import android.provider.MediaStore
+import android.view.View
 import android.view.inputmethod.EditorInfo
 import android.view.inputmethod.InputMethodManager
+import androidx.appcompat.app.AlertDialog
+import androidx.core.app.ActivityCompat
 import androidx.core.view.inputmethod.EditorInfoCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.request.target.CustomTarget
+import com.bumptech.glide.request.transition.Transition
+import com.google.android.material.snackbar.Snackbar
 import com.project.imagesearch.data.Repository
+import com.project.imagesearch.data.models.PhotoResponse
 import com.project.imagesearch.databinding.ActivityMainBinding
 import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.cancel
@@ -27,12 +45,33 @@ class MainActivity : AppCompatActivity() {
 
         initViews()
         bindViews()
-        fetchRandomPhotos()
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            fetchRandomPhotos()
+        } else {
+            requestWriteStoragePermission()
+        }
     }
 
     override fun onDestroy() {
         super.onDestroy()
         scope.cancel()
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        val writeExternalStoragePermissionGranted =
+            requestCode == REQUEST_WRITE_EXTERNAL_STORAGE_PERMISSION &&
+                    grantResults[0] == PackageManager.PERMISSION_GRANTED
+
+        if (writeExternalStoragePermissionGranted) {
+            fetchRandomPhotos()
+        }
     }
 
     private fun initViews() {
@@ -57,16 +96,141 @@ class MainActivity : AppCompatActivity() {
         binding.refreshLayout.setOnRefreshListener {
             fetchRandomPhotos(binding.searchEditText.text.toString())
         }
+        (binding.recyclerView.adapter as? PhotoAdapter)?.onClickPhoto = { photo ->
+            showDownloadPhotoConfirmationDialog(photo)
+        }
+    }
+
+    private fun requestWriteStoragePermission() {
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),
+            REQUEST_WRITE_EXTERNAL_STORAGE_PERMISSION
+        )
     }
 
     @SuppressLint("NotifyDataSetChanged")
     private fun fetchRandomPhotos(query: String? = null) = scope.launch {
-        Repository.getRandomPhotos(query)?.let { photos ->
-            (binding.recyclerView.adapter as? PhotoAdapter)?.apply {
-                this.photos = photos
-                notifyDataSetChanged()
+        try {
+            Repository.getRandomPhotos(query)?.let { photos ->
+                binding.errorDescriptionTextView.visibility = View.GONE
+                (binding.recyclerView.adapter as? PhotoAdapter)?.apply {
+                    this.photos = photos
+                    notifyDataSetChanged()
+                }
             }
+            binding.recyclerView.visibility = View.VISIBLE
+        } catch (exception: Exception) {
+            binding.recyclerView.visibility = View.INVISIBLE
+            binding.errorDescriptionTextView.visibility = View.VISIBLE
+        } finally {
+            binding.shimmerLayout.visibility = View.GONE
             binding.refreshLayout.isRefreshing = false
         }
+    }
+
+    private fun showDownloadPhotoConfirmationDialog(photo: PhotoResponse) {
+        AlertDialog.Builder(this)
+            .setMessage("이 사진을 저장하시겠습니까?")
+            .setPositiveButton("저장") { dialog, _ ->
+                downloadPhoto(photo.urls?.full)
+                dialog.dismiss()
+            }
+            .setNegativeButton("취소") { dialog, _ ->
+                dialog.dismiss()
+            }
+            .create()
+            .show()
+    }
+
+    private fun downloadPhoto(photoUrl: String?) {
+        photoUrl ?: return
+
+
+        Glide.with(this)
+            .asBitmap()
+            .load(photoUrl)
+            .diskCacheStrategy(DiskCacheStrategy.NONE)
+            .into(
+                object: CustomTarget<Bitmap>(SIZE_ORIGINAL, SIZE_ORIGINAL) {
+                    @SuppressLint("ShowToast")
+                    override fun onResourceReady(
+                        resource: Bitmap,
+                        transition: Transition<in Bitmap>?
+                    ) {
+                        saveBitmapToMediaStore(resource)
+
+                        val wallpaperManager = WallpaperManager.getInstance(this@MainActivity)
+                        val snackBar = Snackbar.make(binding.root, "다운로드 완료", Snackbar.LENGTH_SHORT)
+
+                        if ((Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && wallpaperManager.isWallpaperSupported) &&
+                            (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N && wallpaperManager.isSetWallpaperAllowed)) {
+                            snackBar.setAction("배경화면으로 저장") {
+                                try {
+                                    wallpaperManager.setBitmap(resource)
+                                } catch (exception: Exception) {
+                                    Snackbar.make(binding.root, "배경화면 저장 실패", Snackbar.LENGTH_SHORT)
+                                }
+                            }
+                            snackBar.duration = Snackbar.LENGTH_INDEFINITE
+                        }
+                        snackBar.show()
+                    }
+
+                    override fun onLoadStarted(placeholder: Drawable?) {
+                        super.onLoadStarted(placeholder)
+
+                        Snackbar.make(binding.root, "다운로드 중...", Snackbar.LENGTH_INDEFINITE).show()
+                    }
+
+                    override fun onLoadCleared(placeholder: Drawable?) = Unit
+
+                    override fun onLoadFailed(errorDrawable: Drawable?) {
+                        super.onLoadFailed(errorDrawable)
+
+                        Snackbar.make(binding.root, "다운로드 실패", Snackbar.LENGTH_SHORT).show()
+                    }
+                }
+            )
+    }
+
+    private fun saveBitmapToMediaStore(bitmap: Bitmap) {
+        val fileName = "${System.currentTimeMillis()}.jpg"
+        val resolver = applicationContext.contentResolver
+        val imageCollectionUri =
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                MediaStore.Images.Media.getContentUri(
+                    MediaStore.VOLUME_EXTERNAL_PRIMARY
+                )
+            } else {
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+            }
+        val imageDetails = ContentValues().apply {
+            put(MediaStore.Images.Media.DISPLAY_NAME, fileName)
+            put(MediaStore.MediaColumns.MIME_TYPE, "image/jpg")
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                put(MediaStore.Images.Media.IS_PENDING, 1)
+            }
+        }
+
+        val imageUri = resolver.insert(imageCollectionUri, imageDetails)
+
+        imageUri ?: return
+
+        resolver.openOutputStream(imageUri).use { outputStream ->
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream)
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            imageDetails.clear()
+            imageDetails.put(MediaStore.Images.Media.IS_PENDING, 0)
+
+            resolver.update(imageUri, imageDetails, null, null)
+        }
+    }
+
+    companion object {
+        private const val REQUEST_WRITE_EXTERNAL_STORAGE_PERMISSION = 101
     }
 }
